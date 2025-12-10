238e6ea68170504c8d195b10f09a53d63928969c
Brian Le <brianle1301@outlook.com>
2025-12-10 10:41:45 +0000

    use non spa mode for now

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart({
-      spa: {
-        enabled: true,
-      },
-    }),
-    viteReact(),
-    tailwindcss(),
-  ],
+  plugins: [tsConfigPaths(), tanstackStart(), viteReact(), tailwindcss()],
 });
